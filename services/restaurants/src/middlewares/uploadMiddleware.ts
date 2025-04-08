e7028a804dbaed66daabdbf5f8a7814d33d1e80e
Mathieu <mathieu.lemasson@viacesi.fr>
2025-04-08 14:32:08 +0200

    update 100 Mb

--- services/restaurants/src/middlewares/uploadMiddleware.ts
+++ services/restaurants/src/middlewares/uploadMiddleware.ts
@@ -4,7 +4,7 @@ const storage = multer.memoryStorage();
 const upload = multer({
     storage: storage,
     limits: {
-        fileSize: 10 * 1024 * 1024 // Limite à 10MB
+        fileSize: 100 * 1024 * 1024 // Limite à 100Mb
     },
     fileFilter: (req, file, cb) => {
         // Accepter tous types de documents ou filtrer selon vos besoins
